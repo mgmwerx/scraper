bb3411ffb7f7e54ca41f4d278022ea330ebc07e0
zach2017 <zaclewis@gmail.com>
2019-08-26 11:45:59 -0500

    initial spider with raw data need to parse string.
    initial template need to add parse paragraph.

---
+++ scraper/spiders/riverregionjourney-spider.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class RiverRegionJourneySpider(CrawlSpider):
+    name = 'riverregionjourney-spider'
+    allowed_domains = ['www.readjourneymagazine.com']
+    start_urls = ['https://www.readjourneymagazine.com/index.php?option=com_k2&view=item&layout=item&id=222&Itemid=119']
+
+    def parse(self, response):
+        i = 0
+        for div in response.xpath('//p'):
+            item = {}
+            tmp = div.xpath('.//font').get()
+            if ( tmp is not None ):
+              item['title'] = tmp
+              print(item)
+
+              # Parse String from day time -
+              
+              print("------------------------")
+           
+    def parse_details(self, response):
+        detail = {}
+        #print(detail)
+        
+        return detail
