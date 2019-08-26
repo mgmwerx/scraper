902bf1a955c3f177e9ef8f4bdb79114f8a82ca42
zach2017 <zaclewis@gmail.com>
2019-08-25 20:12:15 -0500

    update with title

--- souptry2.py
+++ souptry2.py
@@ -7,6 +7,11 @@ content = urllib2.urlopen(url).read()
 
 soup = BeautifulSoup(content, features="lxml")
 
-print soup
+#print soup
+
 
 print soup.title.string
+for link in soup.find_all('div', class_='mn-title'):
+        print link
+        #print link.get('href')
+     
