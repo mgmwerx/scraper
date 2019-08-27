8f759d4e6b47a1e6b46b589680823784af055519
zach2017 <zaclewis@gmail.com>
2019-08-27 15:10:59 -0500

    add replace control chars and removed request

--- scraper/spiders/lifeatthemax-spider.py
+++ scraper/spiders/lifeatthemax-spider.py
@@ -2,13 +2,9 @@
 import scrapy
 import json
 import re
-import requests
-
-
-
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-
+eventurl = ""
 
 class LifeAtTheMaxSpider(CrawlSpider):
     name = 'lifeatthemax-spider'
@@ -18,18 +14,22 @@ class LifeAtTheMaxSpider(CrawlSpider):
     def parse(self, response):
         i = 0
         # print(response)
+        global eventurl
         for div in response.xpath('//a'):
             item = {}
             item = div.xpath('./@href').get()
-            #print(item)
             eventurl =  "https://www.lifeatthemax.us/" + item
             yield scrapy.Request(eventurl, callback=self.parse_details)
 
     def parse_details(self, response):
         detail = {}
         detail['title'] = response.xpath('//div[@class="jev_evdt_title"]/text()').get()
-        detail['summary'] = response.xpath('//div[@class="jev_evdt_summary"]/text()').get() 
+        detail['url'] = eventurl
+        detail['summary'] = str(response.xpath('//div[@class="jev_evdt_summary"]/text()').get()).replace('\xa0',' ')
         detail['location'] = response.xpath('//div[@class="jev_evdt_location"]/text()').get() 
+        detail['starts_at'] = "N/A"
+        detail['ends_at'] = "N/A"
+        detail['categories'] = "N/A"
         print(detail)
         print("------------------------")
         return detail
