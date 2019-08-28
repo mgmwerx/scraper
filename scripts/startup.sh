dbd3c4cd39c64d2b5cd59b53caa33e9596be6a13
Matias Klein <matias.klein@kognition.ai>
2019-08-28 01:29:51 +0100

    docker startup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -20,7 +20,7 @@ if [ $status -ne 0 ]; then
 fi
 
 # upload egg
-curl http://127.0.0.1:6800/addversion.json -F project=scraper -F version=r23 -F egg=@dist/scraper-1.0-py3.7.egg
+curl http://localhost:6800/addversion.json -F project=scraper -F version=r23 -F egg=@dist/scraper-1.0-py3.7.egg
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to upload egg: $status"
@@ -28,10 +28,10 @@ if [ $status -ne 0 ]; then
 fi
 
 #list spiders
-curl http://127.0.0.1:6800/listspiders.json?project=scraper
+curl http://localhost:6800/listspiders.json?project=scraper
 
 # call each scraper
-curl http://127.0.0.1:6800/schedule.json -d project=scraper -d spider=mgmchamber-spider
+curl http://localhost:6800/schedule.json -d project=scraper -d spider=mgmchamber-spider
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to call MGM Chamber scraper: $status"
