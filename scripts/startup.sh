1d208a7db1698901637e9bc1372eaa39cfbe09fd
Matias Klein <matias.klein@kognition.ai>
2019-08-28 03:04:20 +0100

    docke startup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,6 +3,9 @@
 # turn on bash's job control
 set -m
 
+SERVER_BASE='-scraper.apps.afitc.redhatgov.io'
+URL = "$HOSTNAME$SERVER_BASE"
+
 # Start scrapyd
 python ./app.py &
 status=$?
@@ -20,7 +23,7 @@ if [ $status -ne 0 ]; then
 fi
 
 # upload egg
-curl http://172.30.198.227:6800/addversion.json -F project=scraper -F version=r23 -F egg=@dist/scraper-1.0-py3.7.egg
+curl http://$URL:6800/addversion.json -F project=scraper -F version=r23 -F egg=@dist/scraper-1.0-py3.7.egg
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to upload egg: $status"
@@ -31,11 +34,11 @@ fi
 sleep 5s
 
 #list spiders
-curl http:///172.30.198.227:6800/listspiders.json?project=scraper
+curl http:///$URL:6800/listspiders.json?project=scraper
 
 
 # call each scraper
-curl http:///172.30.198.227:6800/schedule.json -d project=scraper -d spider=mgmchamber-spider
+curl http:///$URL:6800/schedule.json -d project=scraper -d spider=mgmchamber-spider
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to call MGM Chamber scraper: $status"
